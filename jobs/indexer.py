ce67418eea3ae92eef726aa3b4b57d85ac737c00
mrelac <mrelac@ebi.ac.uk>
2020-10-26 13:10:13 +0000

    Oops - joined fusil on the wrong field. Should join on   f.f_ortholog_id, not f.f_id.

--- jobs/indexer.py
+++ jobs/indexer.py
@@ -123,7 +123,7 @@ def get_ortholog_human(spark, df_human):
 def get_ortholog_fusil_mouse_and_human(spark):
     q = '''
     SELECT o.*, f.*, m.*, h.* FROM ortholog o
-    FULL OUTER JOIN fusil f ON f.f_id  = o.o_id
+    FULL OUTER JOIN fusil f ON f.f_ortholog_id  = o.o_id
     FULL OUTER JOIN mouse m ON m.mg_id = o.o_mouse_gene_id
     FULL OUTER JOIN human h ON h.hg_id = o.o_human_gene_id
     '''
